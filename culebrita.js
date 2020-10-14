e6589296d3f95849861c8afac44bd45face07cbc
macadev <danielmacario5@gmail.com>
2020-10-14 08:49:14 -0400

    7. dibujamos a la culebra

--- culebrita.js
+++ culebrita.js
@@ -3,21 +3,6 @@ console.log("Hola, este log viene del archivo culebrita.js");
 let juegoCanvas = document.getElementById("juegoCanvas");
 let ctx = juegoCanvas.getContext("2d");
 
-// ctx.beginPath();
-// ctx.fillStyle = "red";
-// ctx.fillRect(-50, 30, 100, 100);
-// ctx.stroke();
-
-// ctx.font = "40px Arial";
-// ctx.fillStyle = "black";
-// ctx.fillText("Culebrita!", 200, 220);
-
-// ctx.beginPath();
-// ctx.fillStyle = "blue";
-// ctx.ellipse(300, 300, 50, 100, 0, 0, 2 * Math.PI);
-// ctx.fill();
-// ctx.stroke();
-
 for (let x = 20; x < 600; x += 20) {
   ctx.beginPath();
   ctx.fillStyle = "black";
@@ -33,3 +18,8 @@ for (let y = 20; y < 600; y += 20) {
   ctx.lineTo(600, y);
   ctx.stroke();
 }
+
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.fillRect(20, 0, 20, 20);
+ctx.stroke();
