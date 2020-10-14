22b816401848ad4fb720d6b1db74953f75f55f69
macadev <danielmacario5@gmail.com>
2020-10-14 09:04:16 -0400

    8. agrupamos las llamadas para dibujar la culebra y la cuadricula en funciones

--- culebrita.js
+++ culebrita.js
@@ -1,25 +1,33 @@
-console.log("Hola, este log viene del archivo culebrita.js");
-
 let juegoCanvas = document.getElementById("juegoCanvas");
 let ctx = juegoCanvas.getContext("2d");
 
-for (let x = 20; x < 600; x += 20) {
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.moveTo(x, 0);
-  ctx.lineTo(x, 600);
-  ctx.stroke();
+function dibujarCuadricula(context) {
+  for (let x = 20; x < 600; x += 20) {
+    context.beginPath();
+    context.fillStyle = "black";
+    context.moveTo(x, 0);
+    context.lineTo(x, 600);
+    context.stroke();
+  }
+
+  for (let y = 20; y < 600; y += 20) {
+    context.beginPath();
+    context.fillStyle = "black";
+    context.moveTo(0, y);
+    context.lineTo(600, y);
+    context.stroke();
+  }
 }
 
-for (let y = 20; y < 600; y += 20) {
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.moveTo(0, y);
-  ctx.lineTo(600, y);
-  ctx.stroke();
+function dibujarCulebra(context, posX, posY) {
+  context.beginPath();
+  context.fillStyle = "black";
+  context.fillRect(posX, posY, 20, 20);
+  context.stroke();
 }
 
-ctx.beginPath();
-ctx.fillStyle = "black";
-ctx.fillRect(20, 0, 20, 20);
-ctx.stroke();
+dibujarCuadricula(ctx);
+
+dibujarCulebra(ctx, 0, 0);
+dibujarCulebra(ctx, 0, 20);
+dibujarCulebra(ctx, 0, 40);
